b001f6877ebc3a0ac68bd35e9dd889646b1051ad
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2017-01-15 21:59:25 +0100

    Quelques petits tests

---
+++ src/migl2/AllTests.java
@@ -0,0 +1,12 @@
+package migl2;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import migl2.util.ConsListTest;
+
+@RunWith(Suite.class)
+@SuiteClasses(ConsListTest.class)
+public class AllTests {
+}
