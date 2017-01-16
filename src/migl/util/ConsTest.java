1e039bcd7566b53747fb69bc3a4bd490a9acb0ae
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2017-01-16 22:57:05 +0100

    More Cons tests.

--- src/migl/util/ConsTest.java
+++ src/migl/util/ConsTest.java
@@ -1,6 +1,7 @@
 package migl.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
 
@@ -30,6 +31,14 @@ public class ConsTest {
     public void testEquals() {
         Cons<String, String> c1 = new Cons<>("a", "b");
         Cons<String, String> c2 = new Cons<>("a", "b");
+        Cons<String, String> c3 = new Cons<>("a", "c");
+        Cons<String, String> c4 = new Cons<>("c", "b");
+        Cons<String, String> c5 = new Cons<>("c", "d");
         assertEquals(c1, c2);
+        assertNotEquals(c1, null);
+        assertNotEquals(c1, true);
+        assertNotEquals(c1, c3);
+        assertNotEquals(c1, c4);
+        assertNotEquals(c1, c5);
     }
 }
