3a3931b03017587154d82726ab89d9d263bce627
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2017-01-19 10:27:53 +0100

    Some new tests

---
+++ src/migl/util/MoreConsListTest.java
@@ -0,0 +1,71 @@
+package migl.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MoreConsListTest {
+
+    private ConsList<Integer> list;
+
+    @Before
+    public void init() {
+        list = ConsListFactory.nil();
+    }
+
+    @Test(timeout = 20000)
+    public void testPrepend() {
+        for (int i = 0; i < 10000; i++) {
+            list = list.prepend(i);
+        }
+        assertEquals(10000, list.size());
+        ConsList<Integer> list2 = list.prepend(666);
+        assertSame(list, list2.cdr());
+    }
+
+    @Test(timeout = 20000)
+    public void testEqualsEfficiency() {
+        for (int i = 0; i <= 10000; i++) {
+            list = list.prepend(i);
+        }
+        ConsList<Integer> list2 = list.prepend(666);
+        assertEquals(list, list2.cdr());
+    }
+
+    @Test
+    public void testIteratorOnEmptyList() {
+        for (Integer i : list) {
+            fail("there should be no element " + i + " in the list!!!");
+        }
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testIteratorNextOnEmptyList() {
+        list.iterator().next();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testIteratorWithNonEmptyList() {
+        list = list.prepend(4).prepend(3).prepend(2).prepend(1);
+        Iterator<Integer> it = list.iterator();
+        assertTrue(it.hasNext());
+        assertEquals(1, it.next().intValue());
+        assertTrue(it.hasNext());
+        assertEquals(2, it.next().intValue());
+        assertTrue(it.hasNext());
+        assertEquals(3, it.next().intValue());
+        assertTrue(it.hasNext());
+        assertEquals(4, it.next().intValue());
+        assertFalse(it.hasNext());
+        it.next();
+    }
+
+}
