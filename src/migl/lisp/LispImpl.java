fd4f8014f7ac14cba6ad6e0ec96db2c5af00bfde
Heugue Xavier <xavier_heugue@ens.univ-artois.fr>
2017-02-06 14:41:50 +0100

    Is Sonar taking some acid to tell some random errors which takes more line and increase the cyclomatic complexity. And this completely useless but this is a raging comment so it must be long to disturb the corrector mind, which give some fun to the student so he continue his commit message. Just have fun and smell the blood on my keyboard :D

--- src/migl/lisp/LispImpl.java
+++ src/migl/lisp/LispImpl.java
@@ -54,6 +54,8 @@ public class LispImpl implements Lisp {
                     case ')':
                         parentheseLevel--;
                         break;
+                    default:
+                        break;
                     }
                 }
 
