ab6c3aa5deb923d4d823ab15097ba4f8cd9fa0c7
Xavier Heugue <xavier_heugue@ens.univ-artois.fr>
2017-02-11 19:50:00 +0100

    I pass the yeah yeah

--- src/migl/lisp/LispImpl.java
+++ src/migl/lisp/LispImpl.java
@@ -23,7 +23,7 @@ public class LispImpl implements Lisp {
                 try {
                     return LispBoolean.valueOf(expr);
                 } catch (IllegalArgumentException ex3) {
-                    if (expr.contains(")"))
+                    if (expr.contains(")") && !expr.contains("("))
                         throw new LispError("Missing '('");
                     return expr;
                 }
@@ -31,63 +31,60 @@ public class LispImpl implements Lisp {
         }
     }
 
-    private static ConsList<Object> createList(String expr) throws LispError {
+    private static ConsList<Object> parseList(String expr, boolean sublist) throws LispError {
         ConsList<Object> list = ConsListFactory.nil();
+        System.out.println(expr);
         StringBuilder bStr = new StringBuilder();
+        char lastMeetedChar = '\0';
+        int i;
 
-        for (int i = 0; i < expr.length(); i++) {
-            if (expr.charAt(i) == ' ') {
-                String str = bStr.toString();
-                if (!"".equals(str)) {
-                    list = list.append(getEltValue(str));
+        for (i = 1; i < expr.length() && expr.charAt(i) != ')'; i++) {
+            if (expr.charAt(i) == '(') {
+                int fin;
+                for (fin = i; fin < expr.length() && expr.charAt(fin) != ')'; fin++)
+                    ;
+                if (fin == expr.length())
+                    throw new LispError("Missing )");
+                list = list.append(parseList(expr.substring(i, fin + 1), true));
+                i = fin;
+                lastMeetedChar = ')';
+            } else if (expr.charAt(i) == ' ' || expr.charAt(i) == '\t') {
+                if (bStr.length() != 0) {
+                    list = list.append(getEltValue(bStr.toString()));
                     bStr = new StringBuilder();
                 }
-            } else if (expr.charAt(i) == '(' && bStr.toString().length() == 0) {
-                int parentheseLevel = 0;
-                int j;
-                for (j = i + 1; j < expr.length() && !(expr.charAt(j) == ')' && parentheseLevel == 0); j++) {
-                    bStr.append(expr.charAt(j));
-                    switch (expr.charAt(j)) {
-                    case '(':
-                        parentheseLevel++;
-                        break;
-                    case ')':
-                        parentheseLevel--;
-                        break;
-                    default:
-                        break;
-                    }
-                }
-
-                if (expr.length() == j)
-                    throw new LispError("Missing ')'");
-                list = list.append(createList(bStr.toString()));
-                i += bStr.toString().length() + 1;
-                bStr = new StringBuilder();
             } else {
                 bStr.append(expr.charAt(i));
+                lastMeetedChar = expr.charAt(i);
             }
         }
+        if (i == expr.length())
+            throw new LispError("Missing closing parenthesis");
+        else if (expr.length() != i + 1 && !sublist) {
+            for (i += 1; i < expr.length(); i++)
+                if (expr.charAt(i) != ' ' && expr.charAt(i) != '\t')
+                    throw new LispError("Strings contains an unexpected character");
+        } else if (expr.length() != i + 1)
+            throw new LispError("Strings contains an unexpected character");
 
-        if (bStr.toString().length() != 0) {
+        if (bStr.length() != 0)
             list = list.append(getEltValue(bStr.toString()));
-        }
-
         return list;
     }
 
     @Override
     public Object parse(String expr) throws LispError {
-        if (expr.charAt(0) != '(') {
-            return getEltValue(expr);
-        } else {
-            ConsList<Object> list;
-            if (!expr.endsWith(")")) {
-                throw new LispError("Missing ')'");
-            }
-            list = createList(expr.substring(1, expr.length() - 1));
-            return list;
+        if (expr.length() == 0 || expr == null)
+            throw new LispError("Cannot parse empty list");
+        if (expr.charAt(0) == ' ' || expr.charAt(0) == '\t')
+            return parse(expr.substring(1));
+        if (expr.charAt(0) == '(')
+            return parseList(expr, false);
+        if (expr.contains(")")) {
+            throw new LispError("OK...Missing )");
         }
+
+        return getEltValue(expr);
     }
 
     @Override
